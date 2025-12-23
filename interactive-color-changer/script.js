790acf4f5e4916640f1d8c5d7c45fac7fe69e3e1
Eric <erickims08@gmail.com>
2025-12-24 00:37:29 +0300

    chore(interactive-color-changer): comment out console log for generated color

--- interactive-color-changer/script.js
+++ interactive-color-changer/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
-    console.log(color);
+    // console.log(color);
     return color;
   }
 
